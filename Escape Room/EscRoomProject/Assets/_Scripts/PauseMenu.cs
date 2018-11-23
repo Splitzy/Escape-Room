33afcb190f2d6c33ff29b20b176d83a9a6a8a3cd
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2018-11-23 10:48:02 +0000

    Created a pause menu

---
+++ Escape Room/EscRoomProject/Assets/_Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject pauseUI;
+
+    public static bool isPaused = false;
+
+
+
+	// Use this for initialization
+	void Start()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 0f;
+        isPaused = false;
+        Debug.Log("Unpausing Menu...");
+    }
+
+    public void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        Debug.Log("Pausing Menu...");
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
