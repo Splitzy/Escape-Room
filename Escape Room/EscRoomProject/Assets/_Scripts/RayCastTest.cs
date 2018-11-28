bf7f8cff1be0e1fc045cccee4699bfedaacc3af5
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2018-11-28 16:50:10 +0000

    Added a raycast for a prompt (for testing)

---
+++ Escape Room/EscRoomProject/Assets/_Scripts/RayCastTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RayCastTest : MonoBehaviour {
+    public Transform player;
+    public Text promptText;
+    public GameObject promptUI;
+
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray look = new Ray(player.position, player.forward);
+        Debug.DrawRay(player.position, player.forward * 3);
+
+        if (Physics.Raycast(look, out hit, 3))
+        {
+
+            if(hit.collider.tag == "Light")
+            {
+                promptText.text = "This is a light!";
+                promptUI.SetActive(true);
+            }
+            else
+            {
+                promptText.text = "";
+                promptUI.SetActive(false);
+            }
+        }
+	}
+}
